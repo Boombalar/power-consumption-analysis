7dbda997d98fa747e5812b58d12d558d725a1c53
Mathias Fager <Boombalar1@gmail.com>
2021-04-25 19:25:18 +0200

    Load validation

--- app/load.py
+++ app/load.py
@@ -1,15 +1,37 @@
+import numpy as np
 import pandas as pd
 
 
-def load_measurements(filename: str, fmode: str) -> pd.DataFrame:
+def load_measurements(filename: str, fmode: str):
     try:
-        data = pd.read_csv("../data/" + filename)
-        data.columns = ['year', 'month', 'day', 'hour', 'minute', 'second', 'zone1', 'zone2', 'zone3', 'zone4']
-        return data
+        dataset = pd.read_csv("../data/" + filename, header=None)
+        dataset.columns = ['year', 'month', 'day', 'hour', 'minute', 'second', 'zone1', 'zone2', 'zone3', 'zone4']
+
+        if fmode == "drop":
+            dataset = dataset[dataset.select_dtypes(include=[np.number]).ge(0).all(1)]
+        elif fmode == "forward fill":
+            if (dataset.head(1) < 0).any().any():
+                dataset = dataset[dataset.select_dtypes(include=[np.number]).ge(0).all(1)]
+                print("First row had invalid measurements, removed all invalid measurements")
+            else:
+                dataset = dataset.where(dataset.ge(0)).ffill()
+        elif fmode == "backward fill":
+            if (dataset.tail(1) < 0).any().any():
+                dataset = dataset[dataset.select_dtypes(include=[np.number]).ge(0).all(1)]
+                print("First row had invalid measurements, removed all invalid measurements")
+            else:
+                dataset = dataset.where(dataset.ge(0)).bfill()
+
+        tvec = dataset.iloc[:, 0:6]
+        data = dataset.iloc[:, 6:10]
+
+        return tvec, data
     except IOError:
         print(f"File with name {filename} could not be loaded")
     except Exception:
         print("An unexpected error occurred")
 
 
-print(load_measurements("2008.csv", "")["year"])
+print(load_measurements("2008.csv", "forward fill")[1])
+print(load_measurements("2008.csv", "backward fill")[1])
+print(load_measurements("2008.csv", "drop")[1])
