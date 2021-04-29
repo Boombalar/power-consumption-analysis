8f8be5f24940941cc2f3fd89e658c7da4af6d371
outrageousbog <oliverskoeppen@gmail.com>
2021-04-29 10:16:48 +0200

    beautify table print

--- app/statistics.py
+++ app/statistics.py
@@ -8,7 +8,7 @@ def get_quantiles(data) -> []:
     quantiles = [0, .25, .50, .75, 1] # Add more quantiles if needed
     for quantile in quantiles:
         data_quantiles.append(data.quantile(quantile))
-    data_quantiles.append(len(data))
+    data_quantiles.append(str(len(data)))
     return data_quantiles
 
 
@@ -20,17 +20,19 @@ def print_statistics(tvec, data) -> None:
     :param data: An N × 4 matrix where each row is a set of measurements.
     :return: None
     '''
+    print(tvec)
     table_data = []
     table_data.append(get_quantiles(data["zone1"]))
     table_data.append(get_quantiles(data["zone2"]))
     table_data.append(get_quantiles(data["zone3"]))
     table_data.append(get_quantiles(data["zone4"]))
-    columns = ['Minimum', '1. quart.', '2. quart.', '3. quart.', 'Maximum', 'n']
+    columns = ['Minimum\nWh', '1. quart.\nWh', '2. quart.\nWh', '3. quart.\nWh', 'Maximum\nWh', '\nn']
     index = ['Zone 1', 'Zone 2', 'Zone 3', 'Zone 4', 'All Zones']
     table_data.append(get_quantiles(data["zone1"].append(data["zone2"]).append(data["zone3"]).append(data["zone4"])))
     table_data = pd.DataFrame(table_data, index, columns)
-    print(tabulate(table_data, headers=columns, tablefmt='psql'))
+    print(tabulate(table_data, headers=columns, tablefmt='pretty'))
 
-
-
-print_statistics("test", load.load_measurements("2008.csv", ""))
+data = load.load_measurements("2008.csv", "drop")
+# print(load.load_measurements("2008.csv", "drop"))
+print(data)
+print_statistics(data[0], data[1])
