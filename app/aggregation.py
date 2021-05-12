3ccf1c54fd68092d6902c6236af591a189274279
Mathias Fager <Boombalar1@gmail.com>
2021-05-12 14:33:18 +0200

    Correct use of class

--- app/aggregation.py
+++ app/aggregation.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 
-class periods:
-    hour = "hour"
-    day = "day"
-    month = "month"
-    hour_of_the_day = "hour of the day"
+class Periods:
+    HOUR = "hour"
+    DAY = "day"
+    MONTH = "month"
+    HOUR_OF_THE_DAY = "hour of the day"
 
 
-def aggregate_measurements(tvec: pd.DataFrame, data: pd.DataFrame, period: periods):
+def aggregate_measurements(tvec: pd.DataFrame, data: pd.DataFrame, period: Periods):
     dataset = pd.concat([tvec, data], axis=1)
-    if period == periods.hour_of_the_day:
+    if period == Periods.HOUR_OF_THE_DAY:
         data_a = dataset.groupby("hour").mean()
         data_a = data_a.iloc[:, 5:10]
         tvec_a = data_a.index
