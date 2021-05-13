09bb29dacc8d391ecd1bb35f5b4eba30cee1cf5e
outrageousbog <oliverskoeppen@gmail.com>
2021-05-13 09:06:38 +0200

    error handling

--- app/__init__.py
+++ app/__init__.py
@@ -21,16 +21,16 @@ while True:
 
     selected_item = tui.startMenu(data)
 
-    if selected_item == 1:
+    if selected_item == 1: # load / reload data
         aggregated_data = None
         data = tui.loadDataMenu()
-    elif selected_item == 2:
+    elif selected_item == 2: # aggregate
         aggregated_data = tui.aggregateDataMenu(data)
-    elif selected_item == 3:
-        statistics = aggregated_data if aggregated_data is not None else data
+    elif selected_item == 3: # print statistics
+        statistics = aggregated_data if aggregated_data is not None else data  # We know data is always present at this point
         display_statistics.print_statistics(statistics[0], statistics[1])
-    elif selected_item == 4:
-        visuals.plot(data[0], data[1], False)
-    elif selected_item == 9:
+    elif selected_item == 4: # visualize plots
+        tui.visualsMenu(data)
+    elif selected_item == 9: # exit
         print("System is exiting.")
         sys.exit(0)
--- app/tui.py
+++ app/tui.py
@@ -1,11 +1,9 @@
-import pandas as pd
-
 import load
-import statistics
 from termcolor import colored
 # Need this module for colors to work
 import colorama
 import aggregation
+import visuals
 
 # Init the module so it works
 colorama.init()
@@ -18,10 +16,22 @@ Purpose: to extract the navigation from __init__.py into helper methods to keep 
 """
 
 
+def allowed_selection(selection: str, allowed: list) -> None:
+    """
+    Raises #InvalidSelectionException if the input is not among the allowed list
+    :param selection: user input as string
+    :param allowed: allowed user inputs as list of int
+    """
+    for item in allowed:
+        if selection == str(item):
+            return None
+    raise InvalidSelectionException
+
+
 def startMenu(data: tuple) -> int:
     """
     The start menu used to print all the options from the start menu
-    :param data: Boolean to whether load simple menu or advanced menu
+    :param data: (optional) loaded data containing tvec and data
     :return: the number corresponding to the selected menu choice
     """
 
@@ -34,23 +44,20 @@ def startMenu(data: tuple) -> int:
             print(colored("3", "blue") + " Display statistics")
             print(colored("4", "blue") + " Visualize electricity consumption")
         print(colored("9", "red") + " EXIT")
-        return int(input("What do you wish to do? \n"))
+        selection = input("What do you wish to do? \n")
+        allowed_selection(selection, [1, 2, 3, 4, 9])
+        return int(selection)
+    except InvalidSelectionException:
+        print(colored("Invalid selection", "red"))
     except ValueError:
         print("Input could not compute, try again")
         input("Press enter to continue")
 
 
-corruption_mode = {
-    "1": 'forward fill',
-    "2": 'backward fill',
-    "3": 'drop'
-}
-
-
 def loadDataMenu() -> tuple:
     """
-    Method for printing the option to load the dataset, if no selected dataset, use the test.txt in the data folder
-    :return: The dataset in the from of a DataFrame type.
+    Loads user defined csv file located in /data folder
+    :return: The data set as a tuple containing tvec (index 0) and the data (index 1).
     """
     default_file = "2008.csv"
     print("No data is currently loaded.")
@@ -60,19 +67,68 @@ def loadDataMenu() -> tuple:
     print(colored("1", "blue") + " Forward Fill - Replace with latest valid measurement")
     print(colored("2", "blue") + " Backward Fill - Replace with next valid measurement")
     print(colored("3", "blue") + " Drop - Deleted corrupted measurement")
-    fmode = input("Choose one")
     try:
-        mode = corruption_mode[fmode]
+        fmode = input("Choose one")
+        allowed_selection(fmode, [1, 2, 3])
+        mode = corruption_modes[fmode]
         data = load.load_measurements(file_name or default_file, mode)
         if data is None:
             raise Exception
         print(colored("Data was loaded successfully", "green"))
         return data
+    except InvalidSelectionException:
+        print(colored("Invalid selection", "red"))
     except Exception:
         print(colored("Data could not be loaded successfully", "red"))
 
 
-aggregation_mode = {
+def visualsMenu(data):
+    try:
+        print("How should the energy consumption be visualized?")
+        print(colored("1", "blue") + " Individual zones")
+        print(colored("2", "blue") + " Combined zones")
+        combined = input("Choose one")
+        allowed_selection(combined, [1, 2])
+        visuals.plot(data[0], data[1], True if combined == "2" else False)
+    except InvalidSelectionException:
+        print(colored("Invalid selection", "red"))
+
+
+def aggregateDataMenu(data: tuple) -> tuple:
+    """
+    The menu to allow the user to pick an aggregation
+    :param data:
+    :type data: tuple containing tvec and data
+    :return: data: tuple containing aggregated tvec and data
+    """
+    print("How should the data be aggregated?")
+    print(colored("1", "blue") + " Consumption per minute (no aggregation)")
+    print(colored("2", "blue") + " Consumption per hour")
+    print(colored("3", "blue") + " Consumption per day")
+    print(colored("4", "blue") + " Consumption per month")
+    print(colored("5", "blue") + " Hour-of-day consumption (hourly average)")
+    try:
+        amode = input("Choose one")
+        allowed_selection(amode, [1, 2, 3, 4, 5])
+        mode = aggregation_modes[amode]
+        data = aggregation.aggregate_measurements(data[0], data[1], mode)
+        print(colored("Data was aggregated", "green"))
+        return data
+    except InvalidSelectionException:
+        print(colored("Invalid selection", "red"))
+    except Exception as e:
+        print(e)
+        print(colored("Data could not be aggregated", "red"))
+
+
+corruption_modes = {
+    "1": 'forward fill',
+    "2": 'backward fill',
+    "3": 'drop'
+}
+
+
+aggregation_modes = {
     "1": None,
     "2": aggregation.Periods.HOUR,
     "3": aggregation.Periods.DAY,
@@ -81,19 +137,11 @@ aggregation_mode = {
 }
 
 
-def aggregateDataMenu(data: tuple):
-    print("How should the data be aggregated?")
-    print(colored("1", "blue") + " Consumption per minute (no aggregation)")
-    print(colored("2", "blue") + " Consumption per hour")
-    print(colored("3", "blue") + " Consumption per day")
-    print(colored("4", "blue") + " Consumption per month")
-    print(colored("5", "blue") + " Hour-of-day consumption (hourly average)")
-    amode = input("Choose one")
-    try:
-        mode = aggregation_mode[amode]
-        data = aggregation.aggregate_measurements(data[0], data[1], mode)
-        print(colored("Data was aggregated", "green"))
-        return data
-    except Exception as e:
-        print(e)
-        print(colored("Data could not be aggregated", "red"))
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class InvalidSelectionException(Error):
+    """Raised when input is invalid"""
+    pass
